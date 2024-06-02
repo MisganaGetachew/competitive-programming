b38471e5fd202b534c270639d2cf72e5f2bbc330
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-02 21:14:55 +0300

    Created 'B - Rudolph and Cut the Rope ' by A2SV Hub

---
+++ A2SV Remote Contest 16 02-Jun-2024/B - Rudolph and Cut the Rope  163359.py
@@ -0,0 +1,12 @@
+# Problem: B - Rudolph and Cut the Rope  - https://codeforces.com/gym/525787/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    ct = 0
+    for i in range(n):
+        a, b = list(map(int, input().split()))
+        if a > b:
+            ct += 1
+    print(ct)
