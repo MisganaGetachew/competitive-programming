1dbdb0a2c5703f3ac698360924971e8c97428727
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-02 21:15:08 +0300

    Created 'A - Odd One Out' by A2SV Hub

---
+++ A2SV Remote Contest 16 02-Jun-2024/A - Odd One Out 163360.py
@@ -0,0 +1,12 @@
+# Problem: A - Odd One Out - https://codeforces.com/gym/525787/problem/A
+
+from collections import Counter
+t = int(input())
+
+for _ in range(t):
+    c = Counter(list(map(int, input().split())))
+
+    for val, freq in c.items():
+        if freq == 1:
+            print(val)
+            break
