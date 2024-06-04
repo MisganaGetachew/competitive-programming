73b5586694989f59f6fb6770c025408431b8d794
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-04 12:00:44 +0300

    Created 'C - Parity Sort' by A2SV Hub

---
+++ A2SV Remote G5 Contest 17 04-Jun-2024/C - Parity Sort 163995.py
@@ -0,0 +1,20 @@
+# Problem: C - Parity Sort - https://codeforces.com/gym/527307/problem/C
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    newarr = arr.copy()
+    newarr.sort()
+    cnt = 0
+    for i in range(n):
+        if (newarr[i] % 2 == 0 and arr[i] % 2 == 0) or (newarr[i] % 2 != 0 and arr[i] % 2 != 0):
+            cnt += 1
+
+    if cnt == n:
+        print("YES")
+
+    else:
+        print("NO")
