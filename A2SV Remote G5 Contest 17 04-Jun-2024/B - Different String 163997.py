06c8d007709d06a41a7472af4a22bfc1472e71aa
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-04 12:01:15 +0300

    Created 'B - Different String' by A2SV Hub

---
+++ A2SV Remote G5 Contest 17 04-Jun-2024/B - Different String 163997.py
@@ -0,0 +1,15 @@
+# Problem: B - Different String - https://codeforces.com/gym/527307/problem/B
+
+from collections import Counter
+t = int(input())
+
+for _ in range(t):
+    s = input()
+
+    c = Counter(s)
+
+    if len(c.keys()) > 1:
+        print("YES")
+        print(s[1::] + s[0])
+    else:
+        print("NO")
