c7ea20d270083a7f0b75e3ad1d8e0b1a687ec91d
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-04 12:01:18 +0300

    Created 'A - Vlad and the Best of Five' by A2SV Hub

---
+++ A2SV Remote G5 Contest 17 04-Jun-2024/A - Vlad and the Best of Five 163998.py
@@ -0,0 +1,18 @@
+# Problem: A - Vlad and the Best of Five - https://codeforces.com/gym/527307/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    A = 0
+    B = 0
+
+    for i in s:
+        if i == "A":
+            A += 1
+        else:
+            B += 1
+    if A > B:
+        print("A")
+    else:
+        print("B")
