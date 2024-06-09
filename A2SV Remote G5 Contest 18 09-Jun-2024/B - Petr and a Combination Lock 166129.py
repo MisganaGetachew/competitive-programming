6c4dac78609e100a52f97a6b07369bc86e6f58df
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-09 16:45:34 +0300

    Created 'B - Petr and a Combination Lock' by A2SV Hub

---
+++ A2SV Remote G5 Contest 18 09-Jun-2024/B - Petr and a Combination Lock 166129.py
@@ -0,0 +1,17 @@
+# Problem: B - Petr and a Combination Lock - https://codeforces.com/gym/528793/problem/B
+
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+
+def rec(idx, run_sum):
+    if idx == n:
+        return run_sum % 360 == 0
+
+    return rec(idx + 1, run_sum + arr[idx]) or rec(idx + 1, run_sum - arr[idx])
+
+
+print("YES" if rec(0, 0) else "NO")
+
+
+
