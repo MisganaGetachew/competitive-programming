d80f5bc26c0f73e070107fc8be53273ee52d5288
Misgana Getachew <97598321+MisganaGetachew@users.noreply.github.com>
2024-06-09 16:45:22 +0300

    Created 'A - Magic Numbers' by A2SV Hub

---
+++ A2SV Remote G5 Contest 18 09-Jun-2024/A - Magic Numbers 166128.py
@@ -0,0 +1,20 @@
+# Problem: A - Magic Numbers - https://codeforces.com/gym/528793/problem/A
+
+nums = (input())
+res = True
+i = 0
+while i < len(nums):
+    if nums[i:i+3] == "144":
+        i += 3
+    elif nums[i:i+2] == "14":
+        i += 2
+    elif nums[i] == "1":
+        i += 1
+    else:
+        res = False
+        break
+
+if res:
+    print("YES")
+else:
+    print("NO")
