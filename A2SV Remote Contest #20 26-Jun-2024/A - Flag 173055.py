2c1142c510296226849f68a809bc140f8884ec70
Misgana Getachew <misganagetachew111@gmail.com>
2024-06-26 22:18:09 +0300

    Created 'A - Flag' by A2SV Hub

---
+++ A2SV Remote Contest #20 26-Jun-2024/A - Flag 173055.py
@@ -0,0 +1,19 @@
+# Problem: A - Flag - https://codeforces.com/gym/531416/problem/A
+
+def grid():
+    n, m = map(int, input().split())
+    grid = [input().strip() for _ in range(n)]
+    for i in range(n):
+        temp = grid[i][0]
+        for j in range(m):
+            if temp != grid[i][j]:
+                return False
+        if i > 0 and grid[i][0] == grid[i-1][0]:
+            return False
+    return True
+
+
+if grid():
+    print("YES")
+else:
+    print("NO")
