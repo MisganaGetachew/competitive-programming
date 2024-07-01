9135afc0c7260f9cd5990172915ba160ef46d975
Misgana Getachew <misganagetachew111@gmail.com>
2024-07-01 21:55:53 +0300

    Created 'B - New Year Transportation' by A2SV Hub

---
+++ A2SV Remote Contest #21 01-Jul-2024/B - New Year Transportation 175332.py
@@ -0,0 +1,17 @@
+# Problem: B - New Year Transportation - https://codeforces.com/gym/532332/problem/B
+
+
+n, t = map(int, input().strip().split())
+portals = list(map(int, input().strip().split()))
+
+
+cur = 1
+
+while cur < t:
+    cur += portals[cur - 1]
+
+# Check if we have reached the target cell
+if cur == t:
+    print("YES")
+else:
+    print("NO")
