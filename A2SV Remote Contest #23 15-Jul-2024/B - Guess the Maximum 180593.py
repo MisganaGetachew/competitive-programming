b8846ebfc0f41b3f0fec16c5a736ab7da388bef9
Misgana Getachew <misganagetachew111@gmail.com>
2024-07-15 21:31:45 +0300

    Created 'B - Guess the Maximum' by A2SV Hub

---
+++ A2SV Remote Contest #23 15-Jul-2024/B - Guess the Maximum 180593.py
@@ -0,0 +1,12 @@
+# Problem: B - Guess the Maximum - https://codeforces.com/gym/535309/problem/B
+
+t = int(input())
+for _ in range(t):
+     n = int(input())
+     arr = list(map(int, input().split()))
+     ans = max(arr)
+     for i in range(n-1):
+          temp = max(arr[i], arr[i+1])
+          ans = min(ans, temp)
+     print(ans - 1)
+ 
