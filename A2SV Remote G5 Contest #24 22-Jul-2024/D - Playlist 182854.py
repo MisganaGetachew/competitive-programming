f8502c13ec7b028e582ad802771dfeba9046b458
Misgana Getachew <misganagetachew111@gmail.com>
2024-07-22 21:08:08 +0300

    Created 'D - Playlist' by A2SV Hub

---
+++ A2SV Remote G5 Contest #24 22-Jul-2024/D - Playlist 182854.py
@@ -0,0 +1,20 @@
+# Problem: D - Playlist - https://codeforces.com/gym/536373/problem/D
+
+import heapq
+n, k = map(int, input().split())
+s = []
+for _ in range(n):
+    l, b = map(int, input().split())
+    s.append([l, b])
+s.sort(key=lambda x: -x[1])
+heap = []
+ttl = 0
+mx = 0
+for l, b in s:
+    heapq.heappush(heap, l)
+    ttl += l
+    if len(heap) > k:
+        ttl -= heapq.heappop(heap)
+    temp = ttl * b
+    mx = max(mx, temp)
+print(mx)
