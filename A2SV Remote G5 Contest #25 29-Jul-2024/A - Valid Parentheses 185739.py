6b9e3a8e028eb831b6958a0d39e21f2be6ab2380
Misgana Getachew <misganagetachew111@gmail.com>
2024-07-29 21:24:24 +0300

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/A - Valid Parentheses 185739.py
@@ -0,0 +1,18 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+s = input()
+stack = []
+unmatched = 0
+
+for chr in s:
+    if chr == '(':
+        stack.append(chr)
+    elif chr == ')':
+        if stack:
+            stack.pop()
+        else:
+            unmatched += 1
+
+unmatched += len(stack)
+mx = len(s) - unmatched
+print(mx)
