3b72bed9ef2adf06faae7ab26c417492a61c5b9f
Misgana Getachew <misganagetachew111@gmail.com>
2024-07-29 22:05:52 +0300

    Created 'C - Experiment' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 29-Jul-2024/C - Experiment 185784.py
@@ -0,0 +1,20 @@
+# Problem: C - Experiment - https://codeforces.com/gym/537575/problem/C
+
+n = int(input())
+events = []
+
+for _ in range(n):
+    si, ti, bi = map(int, input().split())
+    events.append((si, bi))
+    events.append((ti + 1, -bi))
+events.sort()
+
+current_rooms = 0
+max_rooms = 0
+
+for time, change in events:
+    current_rooms += change
+    if current_rooms > max_rooms:
+        max_rooms = current_rooms
+
+print(max_rooms)
