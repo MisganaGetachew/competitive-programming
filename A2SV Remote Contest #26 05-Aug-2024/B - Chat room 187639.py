1405e053164a83bec0d3a84338cfd60cc84c5b37
Misgana Getachew <misganagetachew111@gmail.com>
2024-08-05 21:42:27 +0300

    Created 'B - Chat room' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/B - Chat room 187639.py
@@ -0,0 +1,17 @@
+# Problem: B - Chat room - https://codeforces.com/gym/540348/problem/B
+
+def func(s):
+    target = "hello"
+    pointer = 0
+
+    for char in s:
+        if char == target[pointer]:
+            pointer += 1
+        if pointer == len(target):
+            return "YES"
+
+    return "NO"
+
+
+s = input()
+print(func(s))
