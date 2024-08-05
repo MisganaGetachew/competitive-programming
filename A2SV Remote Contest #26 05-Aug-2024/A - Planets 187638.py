4414651da2a5ef9b844854e661f37b7d10cf3134
Misgana Getachew <misganagetachew111@gmail.com>
2024-08-05 21:42:16 +0300

    Created 'A - Planets' by A2SV Hub

---
+++ A2SV Remote Contest #26 05-Aug-2024/A - Planets 187638.py
@@ -0,0 +1,22 @@
+# Problem: A - Planets - https://codeforces.com/gym/540348/problem/A
+
+from collections import defaultdict
+
+t = int(input())
+for _ in range(t):
+    n, c = map(int, input().split())
+    orbit = list(map(int, input().split()))
+    graph = defaultdict(int)
+
+    for i in range(n):
+        graph[orbit[i]] += 1
+
+    ans = 0
+
+    for key in graph:
+        if graph[key] > c:
+            # print(graph[key])
+            ans += c
+        else:
+            ans += graph[key]
+    print(ans)
