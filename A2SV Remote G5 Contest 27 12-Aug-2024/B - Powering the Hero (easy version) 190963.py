72bcdc3b77a7e2ff422b7563dffa338f346d6a6f
Misgana Getachew <misganagetachew111@gmail.com>
2024-08-12 21:45:35 +0300

    Created 'B - Powering the Hero (easy version)' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 12-Aug-2024/B - Powering the Hero (easy version) 190963.py
@@ -0,0 +1,17 @@
+# Problem: B - Powering the Hero (easy version) - https://codeforces.com/gym/541399/problem/B
+
+import heapq
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    cards = map(int, input().split())
+    bonuses = []
+    power = 0
+    for num in cards:
+        if num != 0:
+            heapq.heappush(bonuses, -num)
+        else:
+            if bonuses:
+                power += -heapq.heappop(bonuses)
+    print(power)
