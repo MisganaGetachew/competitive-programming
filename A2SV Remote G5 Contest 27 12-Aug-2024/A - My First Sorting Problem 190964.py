1669934cf13683abf9e34755fdde05934bb498cc
Misgana Getachew <misganagetachew111@gmail.com>
2024-08-12 21:45:45 +0300

    Created 'A - My First Sorting Problem' by A2SV Hub

---
+++ A2SV Remote G5 Contest 27 12-Aug-2024/A - My First Sorting Problem 190964.py
@@ -0,0 +1,8 @@
+# Problem: A - My First Sorting Problem - https://codeforces.com/gym/541399/problem/A
+
+t = int(input())
+
+for _ in range(t):
+     num1, num2 = map(int, input().split())
+    #  print("working")
+     print (min(num1, num2), max(num1, num2))
