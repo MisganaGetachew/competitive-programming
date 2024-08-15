95ec42939dfab2fc033274a0481866d7068ae0c9
Misgana Getachew <misganagetachew111@gmail.com>
2024-08-15 21:11:17 +0300

    Time: 1847 ms (6.55%), Space: 164.1 MB (15.70%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo = {}
+        
+        def rec(level, amount):
+            if amount == 0:
+                return 1
+            if level == len(coins):
+                return 0 if amount > 0 else 1
+            key = (level, amount)
+            if key in memo:
+                return memo[key]
+            count = 0
+            for k in range(amount//coins[level] + 1):
+                count += rec(level + 1, amount - k*coins[level])
+            memo[key] = count
+            return count
+        return rec(0, amount)
